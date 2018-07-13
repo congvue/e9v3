08ff4f9ef5f68fc2527a1694ad092b5ac5adcb62
1012041750@qq.com <1012041759@qq.com>
2018-07-13 13:39:51 +0800

    add kernel e9v3 ,rtl8723 wifi ok

---
+++ build.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+#make imx_v6_v7_defconfig
+#make imx_v7_android_defconfig
+#make zImage -j8 KCFLAGS=-mno-android
+cp config_linux .config
+#cp config_android .config
+make dtbs
+make zImage -j20
+cp arch/arm/boot/dts/e9-sabresd.dtb ./
+cp arch/arm/boot/dts/imx6q-sabresd.dtb ./
+cp arch/arm/boot/zImage ./
